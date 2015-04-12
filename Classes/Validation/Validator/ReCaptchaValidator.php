f3e40ac3c4c908ca1de0f16b61eaf4e091fb1f39
Jan Haffner <jan.haffner@iem.thm.de>
2015-04-12 15:04:17 +0200

    [Bug #66010]: Standard error message if no error code is available

--- Classes/Validation/Validator/ReCaptchaValidator.php
+++ Classes/Validation/Validator/ReCaptchaValidator.php
@@ -30,23 +30,27 @@ class ReCaptchaValidator extends \TYPO3\CMS\Extbase\Validation\Validator\Abstrac
         $url = 'https://www.google.com/recaptcha/api/siteverify';
         $apiResponse = json_decode(\TYPO3\CMS\Core\Utility\GeneralUtility::getUrl($url.'?secret='.$secret.'&response='.$value), true);
         if($apiResponse['success'] == FALSE) {
-            foreach($apiResponse['error-codes'] as $errorCode) {
-                switch($errorCode) {
-                    case 'missing-input-secret':
-                        $this->addError($this->translateErrorMessage('missingInputSecret', $extensionName), 1426877004);
-                        break;
-                    case 'invalid-input-secret':
-                        $this->addError($this->translateErrorMessage('invalidInputSecret', $extensionName), 1426877455);
-                        break;
-                    case 'missing-input-response':
-                        $this->addError($this->translateErrorMessage('missingInputResponse', $extensionName), 1426877525);
-                        break;
-                    case 'invalid-input-response':
-                        $this->addError($this->translateErrorMessage('invalidInputResponse', $extensionName), 1426877590);
-                        break;
-                    default:
-                        $this->addError($this->translateErrorMessage('defaultError', $extensionName), 1427031929);
+            if(is_array($apiResponse['error-codes'])){
+                foreach($apiResponse['error-codes'] as $errorCode) {
+                    switch($errorCode) {
+                        case 'missing-input-secret':
+                            $this->addError($this->translateErrorMessage('missingInputSecret', $extensionName), 1426877004);
+                            break;
+                        case 'invalid-input-secret':
+                            $this->addError($this->translateErrorMessage('invalidInputSecret', $extensionName), 1426877455);
+                            break;
+                        case 'missing-input-response':
+                            $this->addError($this->translateErrorMessage('missingInputResponse', $extensionName), 1426877525);
+                            break;
+                        case 'invalid-input-response':
+                            $this->addError($this->translateErrorMessage('invalidInputResponse', $extensionName), 1426877590);
+                            break;
+                        default:
+                            $this->addError($this->translateErrorMessage('defaultError', $extensionName), 1427031929);
+                    }
                 }
+            } else {
+                $this->addError($this->translateErrorMessage('defaultError', $extensionName), 1427031929);
             }
         }
     }
