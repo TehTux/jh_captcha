9fc0290f4241341efe43ae761c00b61d7010516a
Jan Haffner <jan.haffner@iem.thm.de>
2015-04-27 12:10:47 +0200

    Support für TYPO3 7.1.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.2',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.1.99',
 		),
 		'conflicts' => array(
 		),
