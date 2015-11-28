5bd10db71bd55ba92c8ddbb321fc02225c6d1246
Jan Haffner <info@jan-haffner.de>
2015-11-28 14:55:19 +0100

    Name für statisches Template ersetzt

--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,8 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'No CAPTCHA reCAPTCHA');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+	$_EXTKEY,
+	'Configuration/TypoScript',
+	'Google reCAPTCHA v2.0'
+);
