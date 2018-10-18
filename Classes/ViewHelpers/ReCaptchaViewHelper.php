f8f1d16638583154abd2a1021ba798e646856f0d
Jan Haffner <info@jan-haffner.de>
2018-10-18 16:35:51 +0200

    [FEATURE] Support EXT:form (TYPO3 v8+)

--- Classes/ViewHelpers/ReCaptchaViewHelper.php
+++ Classes/ViewHelpers/ReCaptchaViewHelper.php
@@ -47,10 +47,10 @@ class ReCaptchaViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewH
             }
 
             $reCaptcha = '<div id="recaptcha' . $uid . '"></div>';
-            $renderReCaptcha = '<script type="text/javascript">var apiCallback' . $uid . ' = function() { reCaptchaWidget' . $uid . ' = grecaptcha.render("recaptcha' . $uid . '", { "sitekey" : "' . $siteKey .'", "callback" : "captchaCallback' . $uid .'", "theme" : "' . $theme . '", "size" : "' . $size . '" }); }</script>';
-            $reCaptchaApi = '<script src="https://www.google.com/recaptcha/api.js?onload=apiCallback' . $uid . '&hl=' . $lang . '&render=explicit" async defer></script>';
+            $renderReCaptcha = '<script type="text/javascript">var apiCallback' . str_replace("-", "", $this->arguments['uid']) . ' = function() { reCaptchaWidget' . str_replace("-", "", $this->arguments['uid']) . ' = grecaptcha.render("recaptcha' . $uid . '", { "sitekey" : "' . $siteKey .'", "callback" : "captchaCallback' . str_replace("-", "", $this->arguments['uid']) .'", "theme" : "' . $theme . '", "size" : "' . $size . '" }); }</script>';
+            $reCaptchaApi = '<script src="https://www.google.com/recaptcha/api.js?onload=apiCallback' . str_replace("-", "", $this->arguments['uid']) . '&hl=' . $lang . '&render=explicit" async defer></script>';
             if (!$this->arguments['type'] == "powermail") {
-                $callBack = '<script type="text/javascript">var captchaCallback' . $uid . ' = function() { document.getElementById("' . $captchaResponseId . '").value = grecaptcha.getResponse(reCaptchaWidget' . $uid . ') }</script>';
+                $callBack = '<script type="text/javascript">var captchaCallback' . str_replace("-", "", $this->arguments['uid']) . ' = function() { document.getElementById("' . $captchaResponseId . '").value = grecaptcha.getResponse(reCaptchaWidget' . str_replace("-", "", $this->arguments['uid']) . ') }</script>';
             }
 
             return $reCaptcha . $callBack . $renderReCaptcha . $reCaptchaApi;
