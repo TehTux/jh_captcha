b37e19f5928b9b61cd524d9796c7a166c196f16f
Jan Haffner <info@jan-haffner.de>
2018-11-08 10:21:17 +0100

    [TASK] Remove obsolete TYPO3 version check

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,17 +3,15 @@
 ############
 # EXT:form #
 ############
-if (version_compare(\TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version(),
-        "8", ">=")) {
-    # backend language file
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:form/Resources/Private/Language/Database.xlf'][]
-        = 'EXT:jh_captcha/Resources/Private/Language/Backend.xlf';
+# backend language file
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:form/Resources/Private/Language/Database.xlf'][]
+    = 'EXT:jh_captcha/Resources/Private/Language/Backend.xlf';
 
-    # reCaptcha icon
-    $iconRegistry = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $iconRegistry->registerIcon(
-        't3-form-icon-jhcaptcha-recaptcha',
-        \TYPO3\CMS\Core\Imaging\IconProvider\FontawesomeIconProvider::class,
-        ['name' => 'google']
-    );
-}
+# reCaptcha icon
+$iconRegistry = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+    \TYPO3\CMS\Core\Imaging\IconRegistry::class);
+$iconRegistry->registerIcon(
+    't3-form-icon-jhcaptcha-recaptcha',
+    \TYPO3\CMS\Core\Imaging\IconProvider\FontawesomeIconProvider::class,
+    ['name' => 'google']
+);
