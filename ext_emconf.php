b6fdcb40eceb84498bfd9a204156c02498c14322
Joey Bouten <joey@beech.it>
2020-10-02 12:17:55 +0200

    [TASK] Allow 10.4.99 instead of 10.5.99, since thisdoes not exist

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.4',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.5.99',
+            'typo3' => '8.7.0-10.4.99',
         ],
     ],
 ];
