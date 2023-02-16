276142a8616a589375d5aae5ccc30726a91706c0
wrock <wrock@users.noreply.github.com>
2023-02-16 09:14:54 +0100

    Update ReCaptchaViewHelper.php
    bugfix typo callBAck :-)

--- Classes/ViewHelpers/ReCaptchaViewHelper.php
+++ Classes/ViewHelpers/ReCaptchaViewHelper.php
@@ -72,7 +72,7 @@ class ReCaptchaViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractView
         $reCaptcha = '<div id="recaptcha' . $this->arguments['uid'] . '"></div>';
         $renderReCaptcha = '<script type="text/javascript">var apiCallback' . str_replace("-", "", $this->arguments['uid']) . ' = function() { reCaptchaWidget' . str_replace("-", "", $this->arguments['uid']) . ' = grecaptcha.render("recaptcha' . $this->arguments['uid'] . '", { "sitekey" : "' . $siteKey .'", "callback" : "captchaCallback' . str_replace("-", "", $this->arguments['uid']) .'", "theme" : "' . $theme . '", "size" : "' . $size . '" }); }</script>';
         $reCaptchaApi = '<script src="https://www.google.com/recaptcha/api.js?onload=apiCallback' . str_replace("-", "", $this->arguments['uid']) . '&hl=' . $lang . '&render=explicit" async defer></script>';
-        $callBAck = '';
+        $callBack = '';
         if (!$this->isPowermail()) {
             $callBack = '<script type="text/javascript">var captchaCallback' . str_replace("-", "", $this->arguments['uid']) . ' = function() { document.getElementById("' . $captchaResponseId . '").value = grecaptcha.getResponse(reCaptchaWidget' . str_replace("-", "", $this->arguments['uid']) . ') }</script>';
         }
